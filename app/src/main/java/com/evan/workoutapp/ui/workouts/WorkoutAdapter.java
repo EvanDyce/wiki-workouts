a42fd3cf47a60db786261f9f8451fccf42240fa5
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-10-15 11:51:13 -0700

    Starting recycler adapter for workout

--- app/src/main/java/com/evan/workoutapp/ui/workouts/WorkoutAdapter.java
+++ app/src/main/java/com/evan/workoutapp/ui/workouts/WorkoutAdapter.java
@@ -1,2 +1,28 @@
-package com.evan.workoutapp.ui.workouts;public class WorkoutAdapter {
+package com.evan.workoutapp.ui.workouts;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.evan.workoutapp.data.Workout;
+
+import java.util.ArrayList;
+
+public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.Viewholder> {
+
+    private Context context;
+    private ArrayList<Workout> listOfWorkouts;
+
+    public WorkoutAdapter(Context context, ArrayList<Workout> workoutsArrayList) {
+        this.context = context;
+        this.listOfWorkouts = workoutsArrayList;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        private ImageView workoutImageView;
+        private exerciseTitleTextView;
+        
+    }
+
 }
