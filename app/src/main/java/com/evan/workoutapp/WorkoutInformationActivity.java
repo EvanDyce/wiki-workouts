89cee11ff9d34b4c1ac17e4a3b0785930cc38b2a
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-10-29 10:22:26 -0700

    Styles action bar and adds back button

--- app/src/main/java/com/evan/workoutapp/WorkoutInformationActivity.java
+++ app/src/main/java/com/evan/workoutapp/WorkoutInformationActivity.java
@@ -1,9 +1,15 @@
 package com.evan.workoutapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.evan.workoutapp.data.workout.PremadeWorkouts;
@@ -15,9 +21,25 @@ public class WorkoutInformationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_information);
 
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar == null) Log.e("FUCKER", "IT IS NULL");
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D26466")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
         // get intent so I can get teh index that was passed
         Intent intent = getIntent();
         int index = (int) intent.getExtras().get("workout_index");
         Toast.makeText(this, "Workout: " + PremadeWorkouts.getPremadeWorkoutsArraylist().get(index).getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
