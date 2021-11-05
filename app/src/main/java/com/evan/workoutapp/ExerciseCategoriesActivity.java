ebef0936b791d3a1e891fdc07b2572424826f4e9
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-11-05 12:59:12 -0700

    Retrieving proper category exercises and display

--- app/src/main/java/com/evan/workoutapp/ExerciseCategoriesActivity.java
+++ app/src/main/java/com/evan/workoutapp/ExerciseCategoriesActivity.java
@@ -5,19 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.evan.workoutapp.data.Exercises;
 import com.evan.workoutapp.databinding.ActivityExerciseCategoriesBinding;
 
 public class ExerciseCategoriesActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+    private TextView categoryTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_categories);
 
-        ActivityExerciseCategoriesBinding binding = ActivityExerciseCategoriesBinding.inflate(getLayoutInflater());
+        // get teh views in layout
+        categoryTitle = findViewById(R.id.exerciseCategoryName);
+        linearLayout = findViewById(R.id.exercise_linear_layout);
 
+        // get category name that was passed through intent
+        String category = getIntent().getExtras().getString("category");
+
+        for (Exercises.Exercise exercise : Exercises.getMap().get(category)) {
+            TextView temp = new TextView(ExerciseCategoriesActivity.this);
+            temp.setText(exercise.getName());
+            temp.setTextColor(getResources().getColor(R.color.salmon));
+            linearLayout.addView(temp);
+        }
 
     }
 }
