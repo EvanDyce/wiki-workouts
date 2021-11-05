69e29717439f60765113ee8d05a75dcfce2e9d7e
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-11-05 13:36:46 -0700

    Restyling textview text, and adding drawable

--- app/src/main/java/com/evan/workoutapp/ExerciseCategoriesActivity.java
+++ app/src/main/java/com/evan/workoutapp/ExerciseCategoriesActivity.java
@@ -3,18 +3,17 @@ package com.evan.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.evan.workoutapp.data.Exercises;
-import com.evan.workoutapp.databinding.ActivityExerciseCategoriesBinding;
+
+import org.w3c.dom.Text;
 
 public class ExerciseCategoriesActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayout;
+    private LinearLayout rootLinearLayout;
     private TextView categoryTitle;
 
     @Override
@@ -24,16 +23,24 @@ public class ExerciseCategoriesActivity extends AppCompatActivity {
 
         // get teh views in layout
         categoryTitle = findViewById(R.id.exerciseCategoryName);
-        linearLayout = findViewById(R.id.exercise_linear_layout);
+        rootLinearLayout = findViewById(R.id.exercise_linear_layout);
 
         // get category name that was passed through intent
         String category = getIntent().getExtras().getString("category");
 
+        // may have to make traditional for loop to keep index value for onClick
         for (Exercises.Exercise exercise : Exercises.getMap().get(category)) {
             TextView temp = new TextView(ExerciseCategoriesActivity.this);
             temp.setText(exercise.getName());
-            temp.setTextColor(getResources().getColor(R.color.salmon));
-            linearLayout.addView(temp);
+            temp.setTextColor(getColor(R.color.black));
+            temp.setTextSize(22.0f);
+            temp.setPadding(0, 0, 0, 10);
+
+            // setting the drawable bullet point to the left of exercises
+            temp.setCompoundDrawablesWithIntrinsicBounds(R.drawable.black_circle_bullet, 0, 0, 0);
+            temp.setCompoundDrawablePadding(5);
+            
+            rootLinearLayout.addView(temp);
         }
 
     }
