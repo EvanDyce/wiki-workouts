b5ef5c662d470f637310f4d9147b6945d9178701
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-11-24 14:55:24 -0800

    Starting to make the layout

--- app/src/main/java/com/evan/workoutapp/MakeCustomWorkoutActivity.java
+++ app/src/main/java/com/evan/workoutapp/MakeCustomWorkoutActivity.java
@@ -1,14 +1,51 @@
 package com.evan.workoutapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.evan.workoutapp.databinding.ActivityMakeCustomWorkoutBinding;
 
 public class MakeCustomWorkoutActivity extends AppCompatActivity {
+
+    private ActivityMakeCustomWorkoutBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_make_custom_workout);
+
+        binding = ActivityMakeCustomWorkoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActionBar actionBar = getSupportActionBar();
+
+        // get the action bar and set teh color and home button
+        if (actionBar == null) Log.e("FUCKER", "IT IS NULL");
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D26466")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * on click for the back button on action bar
+     * @param item menuitem that was clicked, always back button
+     * @return returns true
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(MakeCustomWorkoutActivity.this, MainActivity.class);
+            intent.putExtra("fragment", MainActivity.CUSTOM_WORKOUT_FRAGMENT);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
