1122f6855379da8c837f1fdca2db489923c483f2
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-11-24 14:05:20 -0800

    Adds custom workout creation activity

---
+++ app/src/main/java/com/evan/workoutapp/MakeCustomWorkoutActivity.java
@@ -0,0 +1,14 @@
+package com.evan.workoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeCustomWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_custom_workout);
+    }
+}
