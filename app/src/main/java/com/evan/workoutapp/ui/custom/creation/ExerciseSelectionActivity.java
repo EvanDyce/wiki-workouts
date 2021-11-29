6874cdfde5ee6f27ac913c2a22a25c6009675796
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-11-29 11:50:42 -0800

    Creates and impmlements filtering interface in exercise fragment. Creates ExerciseSelectionActivity

--- app/src/main/java/com/evan/workoutapp/ui/custom/creation/ExerciseSelectionActivity.java
+++ app/src/main/java/com/evan/workoutapp/ui/custom/creation/ExerciseSelectionActivity.java
@@ -7,22 +7,27 @@ import android.os.Bundle;
 import com.evan.workoutapp.R;
 import com.evan.workoutapp.data.Exercises;
 import com.evan.workoutapp.databinding.ActivityExerciseSelectionBinding;
+import com.evan.workoutapp.databinding.ActivityMainBinding;
+import com.evan.workoutapp.ui.ExerciseFilteringInterface;
 import com.evan.workoutapp.ui.exercises.ExerciseAdapter;
 
 import java.util.ArrayList;
 
-public class ExerciseSelectionActivity extends AppCompatActivity {
+public class ExerciseSelectionActivity extends AppCompatActivity implements ExerciseFilteringInterface {
 
     private String currentCategoryFilter = "All";
 
     private ActivityExerciseSelectionBinding binding;
     private final String TAG = "Exercise_Selection";
     private ArrayList<Exercises.Exercise> exerciseArrayList;
+    private ExerciseAdapter exerciseAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise_selection);
+
+        binding = ActivityExerciseSelectionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
