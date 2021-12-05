3d35de1670d476677bf9da4c4756a46b4097d44c
Evan Dyce <endyce@gmail.com>
2021-12-04 18:20:13 -0800

    Proper retrieval of custom_exercises in CurrentUserSingleton

--- app/src/main/java/com/evan/workoutapp/user/CurrentUserSingleton.java
+++ app/src/main/java/com/evan/workoutapp/user/CurrentUserSingleton.java
@@ -1,9 +1,12 @@
 package com.evan.workoutapp.user;
 
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
+import com.evan.workoutapp.data.Exercises;
 import com.evan.workoutapp.data.workout.Workout;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -13,6 +16,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CurrentUserSingleton {
@@ -31,6 +35,7 @@ public class CurrentUserSingleton {
             db.collection("users")
                     .document(email).get()
                     .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @RequiresApi(api = Build.VERSION_CODES.N)
                         @Override
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                             if (task.isSuccessful()) {
@@ -40,7 +45,35 @@ public class CurrentUserSingleton {
                                     String email = (String) map.get("email");
                                     String name = (String) map.get("name");
                                     int workouts_completed = ((Long) map.get("workouts_completed")).intValue();
-                                    ArrayList<Workout> workouts = (ArrayList<Workout>) map.get("custom_workouts");
+                                    ArrayList<Workout> workouts = new ArrayList<>();
+
+                                    // retrieving and adding all the workouts
+                                    ArrayList<HashMap<String, Object>> mapList = (ArrayList<HashMap<String, Object>>) document.get("custom_workouts");
+                                    for (HashMap<String, Object> mappy : mapList) {
+                                        String category = (String) mappy.get("category");
+                                        String description = (String) mappy.get("description");
+                                        String workout_name = (String) mappy.get("name");
+                                        String primary = (String) mappy.get("primary");
+                                        String secondary = (String) mappy.get("secondary");
+                                        String difficulty = (String) mappy.get("difficulty");
+                                        String length = (String) mappy.get("length");
+                                        ArrayList<Exercises.Exercise> exerciseArrayList = new ArrayList<>();
+
+                                        // retrieve the arraylist of map exercises and then create teh individual exercises to add
+                                        ArrayList<HashMap<String, Object>> exercises = (ArrayList<HashMap<String, Object>>) mappy.getOrDefault("exercises", new ArrayList<>());
+                                        for (HashMap<String, Object> exercise : exercises) {
+                                            String exercise_id = (String) exercise.get("id");
+                                            String exercise_name = (String) exercise.get("name");
+                                            String exercise_description = (String) exercise.get("description");
+                                            String exercise_category = (String) exercise.get("category");
+                                            String exercise_equipment = (String) exercise.get("equipment");
+                                            exerciseArrayList.add(new Exercises.Exercise(exercise_id, exercise_name, exercise_description,
+                                                    exercise_category, exercise_equipment));
+                                        }
+                                        workouts.add(new Workout(workout_name, category, primary, secondary, description,
+                                                difficulty, length, exerciseArrayList));
+                                    }
+
                                     instance = new User(name, email, workouts, workouts_completed);
                                 } else {
                                     Log.e(TAG, "Document doesn't exist");
