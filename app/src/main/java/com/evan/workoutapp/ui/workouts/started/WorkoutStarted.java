cadd666930392aca9c015f147f2fc3439c46149a
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-12-08 14:41:59 -0800

    Starting to work on start activity layout

---
+++ app/src/main/java/com/evan/workoutapp/ui/workouts/started/WorkoutStarted.java
@@ -0,0 +1,16 @@
+package com.evan.workoutapp.ui.workouts.started;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.evan.workoutapp.R;
+
+public class WorkoutStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_started);
+    }
+}
