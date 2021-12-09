cfc72cab64a3938813e819a46cbe52462442b10d
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-12-09 13:41:23 -0800

    Styling action bar and some changes to layout

--- app/src/main/java/com/evan/workoutapp/ui/workouts/started/WorkoutStarted.java
+++ app/src/main/java/com/evan/workoutapp/ui/workouts/started/WorkoutStarted.java
@@ -1,16 +1,33 @@
 package com.evan.workoutapp.ui.workouts.started;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.evan.workoutapp.R;
+import com.evan.workoutapp.databinding.ActivityWorkoutInformationBinding;
+import com.evan.workoutapp.databinding.ActivityWorkoutStartedBinding;
 
 public class WorkoutStarted extends AppCompatActivity {
+
+    private ActivityWorkoutStartedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout_started);
+        binding = ActivityWorkoutStartedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActionBar actionBar = getSupportActionBar();
+
+        // get the action bar and set teh color and home button
+        if (actionBar == null) Log.e("FUCKER", "IT IS NULL");
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D26466")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
