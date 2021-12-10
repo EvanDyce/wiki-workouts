fef8815a765a3c79cf2ea245d815d2757832f037
EvanDyce <74132466+EvanDyce@users.noreply.github.com>
2021-12-10 10:53:09 -0800

    Stopwatch working

--- app/src/main/java/com/evan/workoutapp/ui/workouts/started/WorkoutStartedActivity.java
+++ app/src/main/java/com/evan/workoutapp/ui/workouts/started/WorkoutStartedActivity.java
@@ -1,20 +1,34 @@
 package com.evan.workoutapp.ui.workouts.started;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.evan.workoutapp.R;
+import com.evan.workoutapp.data.Exercises;
 import com.evan.workoutapp.databinding.ActivityWorkoutInformationBinding;
 import com.evan.workoutapp.databinding.ActivityWorkoutStartedBinding;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class WorkoutStartedActivity extends AppCompatActivity {
 
     private ActivityWorkoutStartedBinding binding;
+
+    private ArrayList<Exercises.Exercise> exerciseArrayList;
+    private int exerciseCardIndex;
+
+    private int seconds;
+    private boolean is_running, was_running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +43,72 @@ public class WorkoutStartedActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#D26466")));
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        is_running = true;
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            is_running = savedInstanceState.getBoolean("isRunning");
+            was_running = savedInstanceState.getBoolean("wasRunning");
+        }
+
+        binding.tvStopwatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStopwatchClicked();
+            }
+        });
+        startTimer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+        outState.putBoolean("isRunning", is_running);
+        outState.putBoolean("wasRunning", was_running);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        was_running = is_running;
+        is_running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (was_running) {
+            is_running = true;
+        }
+    }
+
+    private void onStopwatchClicked() {
+        is_running = !is_running;
+    }
+
+    private void startTimer() {
+        TextView textView = binding.tvStopwatch;
+        Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int mins = seconds / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.CANADA, "    %02d:%02d    ",
+                        mins, secs);
+
+                textView.setText(time);
+
+                if (is_running) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
